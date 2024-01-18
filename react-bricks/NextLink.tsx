2472148d227041a317cd03a4cb5d406cd1053e07
Kevin Ren <work.kevin.ren@gmail.com>
2024-01-17 18:12:01 -0800

    fixed formatting in react-bricks/config and fixed no prop type error in NextLink.tsx

--- react-bricks/NextLink.tsx
+++ react-bricks/NextLink.tsx
@@ -2,13 +2,21 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { types } from 'react-bricks/frontend'
 
+interface NextLinkProps {
+  href: string
+  className: string
+  activeClassName: string
+  isAdmin: boolean
+  children: React.ReactNode
+}
+
 const NextLink: types.RenderLocalLink = ({
   href,
   className,
   activeClassName,
   isAdmin,
   children,
-}) => {
+}: NextLinkProps) => {
   const router = useRouter()
 
   let anchorClassName = ''
--- react-bricks/config.ts
+++ react-bricks/config.ts
@@ -1,9 +1,9 @@
-import Router from "next/router"
-import { types } from "react-bricks/frontend"
+import Router from 'next/router'
+import { types } from 'react-bricks/frontend'
 
-import bricks from "./bricks"
-import pageTypes from "./pageTypes"
-import NextLink from "./NextLink"
+import bricks from './bricks'
+import pageTypes from './pageTypes'
+import NextLink from './NextLink'
 
 const config: types.ReactBricksConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
@@ -12,33 +12,33 @@ const config: types.ReactBricksConfig = {
   bricks,
   pageTypes,
   customFields: [],
-  logo: "/logo.svg",
+  logo: '/logo.svg',
   loginUI: {},
-  contentClassName: "",
-  //defaultTheme: "",
+  contentClassName: '',
+  // defaultTheme: "",
   renderLocalLink: NextLink,
   navigate: (path: string) => Router.push(path),
-  loginPath: "/admin",
-  editorPath: "/admin/editor",
-  playgroundPath: "/admin/playground",
-  appSettingsPath: "/admin/app-settings",
-  previewPath: "/preview",
-  //getAdminMenu: () => [],
+  loginPath: '/admin',
+  editorPath: '/admin/editor',
+  playgroundPath: '/admin/playground',
+  appSettingsPath: '/admin/app-settings',
+  previewPath: '/preview',
+  // getAdminMenu: () => [],
   isDarkColorMode: false,
   toggleColorMode: () => {},
   useCssInJs: false,
-  appRootElement: "#__next",
+  appRootElement: '#__next',
   clickToEditSide: types.ClickToEditSide.BottomRight,
-  //responsiveBreakpoints: [{ type: types.DeviceType.Phone, width: 480, label: "Smartphone" },],
+  // responsiveBreakpoints: [{ type: types.DeviceType.Phone, width: 480, label: "Smartphone" },],
   enableAutoSave: true,
   disableSaveIfInvalidProps: false,
   enablePreview: true,
   blockIconsPosition: types.BlockIconsPosition.OutsideBlock,
   enableUnsplash: true,
-  unsplashApiKey: "",
+  unsplashApiKey: '',
   enablePreviewImage: true,
   enableDefaultEmbedBrick: true,
-  //permissions,  Fine-grained permissions for enterprise plans
+  // permissions,  Fine-grained permissions for enterprise plans
 }
 
 export default config
